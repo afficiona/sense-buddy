b71b0cdef5153c2c8e633106ca462c95a31107a8
Aman Pandey <aman.pandey@rksv.in>
2019-06-10 21:09:06 +0530

    fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,6 @@
 // Url for the API
-export const USER_DATA_FETCH_API = process.env.NODE_ENV === production ? 'https://sense-buddy.herokuapp.com' : 'http://localhost:5000';
+export const USER_DATA_FETCH_API =
+  process.env.NODE_ENV === 'production' ?
+  'https://sense-buddy.herokuapp.com' :
+  'http://localhost:5000'
+;
